30620323674002655b0c31d9daadda35614c9ba8
Markus Ackermann <markus.ackermann@europace.de>
2021-04-28 22:02:39 +0200

    Provide some demo data on startup

--- src/main/kotlin/com/kaixo/todoist/TodoistApplication.kt
+++ src/main/kotlin/com/kaixo/todoist/TodoistApplication.kt
@@ -1,10 +1,28 @@
 package com.kaixo.todoist
 
+import com.kaixo.todoist.model.ItemState
+import com.kaixo.todoist.model.TodoItem
+import com.kaixo.todoist.persistence.TodoItemRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.stream.Stream
+
 
 @SpringBootApplication
-class TodoistApplication
+class TodoistApplication {
+	@Bean
+	fun init(todoItemRepository: TodoItemRepository): CommandLineRunner {
+		return CommandLineRunner { args: Array<String?>? ->
+			Stream.of("Buy milk", "Call mom", "Bring out the rubbish").forEach { title: String ->
+				val item = TodoItem(title = title, description = "dobidoobidoo", state = ItemState.TODO)
+				todoItemRepository.save(item)
+			}
+			todoItemRepository.findAll().forEach(System.out::println)
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<TodoistApplication>(*args)
