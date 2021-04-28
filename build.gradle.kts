ea132155e1f1b5f798ce3dec8de02b66b9caf2dd
Markus Ackermann <markus.ackermann@europace.de>
2021-04-28 20:14:17 +0200

    Oops, we need the spring-data-jpa dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencyManagement {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("io.crnk:crnk-setup-spring-boot2")
 	implementation("io.crnk:crnk-format-plain-json")
