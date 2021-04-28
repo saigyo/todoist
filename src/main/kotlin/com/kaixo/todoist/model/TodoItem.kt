00c6e1e7eedf8913bc197e2f4b65c937905db24c
Markus Ackermann <markus.ackermann@europace.de>
2021-04-28 20:14:53 +0200

    Declare TodoItem as Entity and JSON:API Resource

--- src/main/kotlin/com/kaixo/todoist/model/TodoItem.kt
+++ src/main/kotlin/com/kaixo/todoist/model/TodoItem.kt
@@ -1,6 +1,18 @@
 package com.kaixo.todoist.model
 
-data class TodoItem(var id: Long,
-                    var title: String,
-                    var description: String,
-                    var state: ItemState = ItemState.TODO)
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.crnk.core.resource.annotations.JsonApiId
+import io.crnk.core.resource.annotations.JsonApiResource
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+@JsonApiResource(type="todoItems")
+data class TodoItem(@JsonApiId
+                    @GeneratedValue(strategy = GenerationType.AUTO)
+                    @Id var id: Long? = null,
+                    @JsonProperty var title: String? = null,
+                    @JsonProperty var description: String? = null,
+                    @JsonProperty var state: ItemState? = null)
