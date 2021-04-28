ac375f3a226529e153664aa6e4c5dacf220e2bcd
Markus Ackermann <markus.ackermann@europace.de>
2021-04-28 22:03:14 +0200

    CorsConfig to allow requests from ng dev server running on :4200

---
+++ src/main/kotlin/com/kaixo/todoist/CorsConfig.kt
@@ -0,0 +1,26 @@
+package com.kaixo.todoist
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import javax.servlet.Filter
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<*> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("http://localhost:4200")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean: FilterRegistrationBean<*> = FilterRegistrationBean<Filter>(CorsFilter(source))
+        bean.order = 0
+        return bean
+    }
+}
