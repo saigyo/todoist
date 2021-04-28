506cd02ed0044830c038248ca127ea1c582773db
Markus Ackermann <markus.ackermann@europace.de>
2021-04-28 20:32:59 +0200

    Add todoitems component

--- todoist-app/src/app/app.module.ts
+++ todoist-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoitemsComponent } from './todoitems/todoitems.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoitemsComponent
   ],
   imports: [
     BrowserModule
---
+++ todoist-app/src/app/todoitems/todoitems.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todoitems',
+  templateUrl: './todoitems.component.html',
+  styleUrls: ['./todoitems.component.css']
+})
+export class TodoitemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
